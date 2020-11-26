edc9cd8424fc339c8dbc4b0fa0567fb103200aa9
osamamagdy <59124937+osamamagdy@users.noreply.github.com>
2020-11-26 17:30:42 +0200

    all files

---
+++ DBapplication/DBapplication/Supplier.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace DBapplication
+{
+    partial class Supplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insertButton = new System.Windows.Forms.Button();
+            this.sNumTextBox = new System.Windows.Forms.TextBox();
+            this.sNameTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sNumDelTextBox = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.sNumUpdateTextBox = new System.Windows.Forms.TextBox();
+            this.cityUpdateTextBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.suppliersDataGrid = new System.Windows.Forms.DataGridView();
+            this.selectAllButton = new System.Windows.Forms.Button();
+            this.countButton = new System.Windows.Forms.Button();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.suppliersDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(554, 63);
+            this.insertButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(165, 39);
+            this.insertButton.TabIndex = 0;
+            this.insertButton.Text = "Insert Supplier";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // sNumTextBox
+            // 
+            this.sNumTextBox.Location = new System.Drawing.Point(58, 34);
+            this.sNumTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sNumTextBox.Name = "sNumTextBox";
+            this.sNumTextBox.Size = new System.Drawing.Size(148, 26);
+            this.sNumTextBox.TabIndex = 1;
+            // 
+            // sNameTextBox
+            // 
+            this.sNameTextBox.Location = new System.Drawing.Point(346, 34);
+            this.sNameTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sNameTextBox.Name = "sNameTextBox";
+            this.sNameTextBox.Size = new System.Drawing.Size(148, 26);
+            this.sNameTextBox.TabIndex = 2;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(58, 109);
+            this.cityTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(148, 26);
+            this.cityTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "S#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(261, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Status";
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Location = new System.Drawing.Point(346, 109);
+            this.statusTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.Size = new System.Drawing.Size(148, 26);
+            this.statusTextBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 202);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "S#";
+            // 
+            // sNumDelTextBox
+            // 
+            this.sNumDelTextBox.Location = new System.Drawing.Point(58, 197);
+            this.sNumDelTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sNumDelTextBox.Name = "sNumDelTextBox";
+            this.sNumDelTextBox.Size = new System.Drawing.Size(148, 26);
+            this.sNumDelTextBox.TabIndex = 10;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(346, 194);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(148, 37);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Delete Supplier";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 301);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "S#";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(261, 301);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "City";
+            // 
+            // sNumUpdateTextBox
+            // 
+            this.sNumUpdateTextBox.Location = new System.Drawing.Point(58, 298);
+            this.sNumUpdateTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sNumUpdateTextBox.Name = "sNumUpdateTextBox";
+            this.sNumUpdateTextBox.Size = new System.Drawing.Size(148, 26);
+            this.sNumUpdateTextBox.TabIndex = 14;
+            // 
+            // cityUpdateTextBox
+            // 
+            this.cityUpdateTextBox.Location = new System.Drawing.Point(346, 298);
+            this.cityUpdateTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cityUpdateTextBox.Name = "cityUpdateTextBox";
+            this.cityUpdateTextBox.Size = new System.Drawing.Size(148, 26);
+            this.cityUpdateTextBox.TabIndex = 15;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(554, 294);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(165, 35);
+            this.updateButton.TabIndex = 16;
+            this.updateButton.Text = "Update Supplier";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(646, 660);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(164, 35);
+            this.closeButton.TabIndex = 17;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // suppliersDataGrid
+            // 
+            this.suppliersDataGrid.AllowUserToAddRows = false;
+            this.suppliersDataGrid.AllowUserToDeleteRows = false;
+            this.suppliersDataGrid.AllowUserToOrderColumns = true;
+            this.suppliersDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.suppliersDataGrid.Location = new System.Drawing.Point(22, 455);
+            this.suppliersDataGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.suppliersDataGrid.Name = "suppliersDataGrid";
+            this.suppliersDataGrid.ReadOnly = true;
+            this.suppliersDataGrid.Size = new System.Drawing.Size(788, 195);
+            this.suppliersDataGrid.TabIndex = 18;
+            // 
+            // selectAllButton
+            // 
+            this.selectAllButton.Location = new System.Drawing.Point(555, 393);
+            this.selectAllButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectAllButton.Name = "selectAllButton";
+            this.selectAllButton.Size = new System.Drawing.Size(164, 35);
+            this.selectAllButton.TabIndex = 19;
+            this.selectAllButton.Text = "Select All Suppliers";
+            this.selectAllButton.UseVisualStyleBackColor = true;
+            this.selectAllButton.Click += new System.EventHandler(this.selectAllButton_Click);
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(346, 393);
+            this.countButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(150, 35);
+            this.countButton.TabIndex = 20;
+            this.countButton.Text = "Count Suppliers";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Enabled = false;
+            this.countTextBox.Location = new System.Drawing.Point(150, 397);
+            this.countTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.ReadOnly = true;
+            this.countTextBox.Size = new System.Drawing.Size(148, 26);
+            this.countTextBox.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 405);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 20);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Suppliers Count";
+            // 
+            // Supplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 724);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.selectAllButton);
+            this.Controls.Add(this.suppliersDataGrid);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.cityUpdateTextBox);
+            this.Controls.Add(this.sNumUpdateTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.sNumDelTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.sNameTextBox);
+            this.Controls.Add(this.sNumTextBox);
+            this.Controls.Add(this.insertButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Supplier";
+            this.Text = "Supplier";
+            this.Load += new System.EventHandler(this.Supplier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.suppliersDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.TextBox sNumTextBox;
+        private System.Windows.Forms.TextBox sNameTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox sNumDelTextBox;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox sNumUpdateTextBox;
+        private System.Windows.Forms.TextBox cityUpdateTextBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.DataGridView suppliersDataGrid;
+        private System.Windows.Forms.Button selectAllButton;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.TextBox countTextBox;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ DBapplication/DBapplication/Supplier.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public partial class Supplier : Form
+    {
+        Controller controllerObj;
+        public Supplier()
+        {
+            InitializeComponent();
+        }
+
+        private void Supplier_Load(object sender, EventArgs e)
+        {
+            controllerObj = new Controller();
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            // It is good to use Int16.TryParse first to check if the entered 'status' is a number
+            // You should valaidate from the beginning that entered value to be integer.
+            int result = controllerObj.InsertSupplier(sNumTextBox.Text, sNameTextBox.Text, cityTextBox.Text, Int16.Parse(statusTextBox.Text));
+            if (result == 0)
+            {
+                MessageBox.Show("The insertion of a new Supplier failed");
+            }
+            else
+            {
+                MessageBox.Show("The row is inserted successfully!");
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            int result = controllerObj.DeleteSupplier(sNumDelTextBox.Text);
+            if (result == 0)
+            {
+                MessageBox.Show("No rows are deleted");
+            }
+            else
+            {
+                MessageBox.Show("The row is deleted successfully!");
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            int result = controllerObj.UpdateSupplier(sNumUpdateTextBox.Text, cityUpdateTextBox.Text);
+            if (result == 0)
+            {
+                MessageBox.Show("No rows are updated");
+            }
+            else
+            {
+                MessageBox.Show("The row is updated successfully");
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            controllerObj.TerminateConnection();
+            this.Close();
+        }
+
+        private void selectAllButton_Click(object sender, EventArgs e)
+        {
+            DataTable dt = controllerObj.SelectAllSuppliers();
+            suppliersDataGrid.DataSource = dt;
+            suppliersDataGrid.Refresh();
+
+
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            countTextBox.Text = controllerObj.CountSuppliers().ToString();
+        }
+    }
+}
